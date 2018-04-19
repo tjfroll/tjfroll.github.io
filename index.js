9265cbab3d01658cd4701ceb7ad3f14a3b6ba980
Tim Froehlich <tjfroll@gmail.com>
2018-04-19 00:13:08 -0400

    Favicon, about tab, convert to html, semantic tags

--- index.js
+++ index.js
@@ -1,59 +1,25 @@
+const tabs = {}
+const navTabs = {}
 
+let current = 'experiments'
 
-append(document.body, new Header('.header', [
-  social,
-  new H1({
-    textContent: 'tim froehlich'
-  })
-]))
+const setActive = (currentTab, currentNavTab) => {
+  for (const text in tabs) {
+    const tab = tabs[text]
+    const isCurrent = tab === currentTab
+    tab.classList.toggle('active', isCurrent)
+  }
+  for (const text in navTabs) {
+    const tab = navTabs[text]
+    const isCurrent = tab === currentNavTab
+    tab.classList.toggle('active', isCurrent)
+  }
+}
 
-const selectedTab = reactive('experiments')
-append(document.body, new Nav('.tabs', [
-  A('.active', {
-    textContent: 'experiments',
-    title: 'experiments',
-    classes: {
-      active: selectedTab.to(tab => tab === 'experiments')
-    },
-    onclick: (e) => selectedTab.put('experiments')
-  }),
-  A('', {
-    textContent: 'work',
-    title: 'work',
-    classes: {
-      active: selectedTab.to(tab => tab === 'work')
-    },
-    onclick: (e) => selectedTab.put('work')
-  }),
-  A('', {
-    textContent: 'about',
-    title: 'about',
-    classes: {
-      active: selectedTab.to(tab => tab === 'about')
-    },
-    onclick: (e) => selectedTab.put('about')
-  }),
-  Span('.spacer'),
-]))
-const container = append(document.body, new Div('.container', [
-  new Article('.tab', {
-    display: selectedTab.to(tab => tab === 'experiments'),
-    // In order for the project names to appear on top of experiments lower on the list,
-    // we reverse the order of the array and use reverse flex-direction to maintain desired order
-    content: experiments.reverse().map(prj => new Project(prj))
-  }),
-  new Article('.tab', {
-    display: selectedTab.to(tab => tab === 'work')
-  }),
-  new Article('.tab', {
-    display: selectedTab.to(tab => tab === 'about'),
-    content: [
-      Div([
-        Img('.bio-img', {
-          src: './img/bio.jpg',
-          width: '180px',
-        })
-      ])
-    ]
-  })
-]))
+for (const tabText of ['experiments', 'about']) {
+  const currentTab = tabs[tabText] || (tabs[tabText] = document.getElementById(`${tabText}-tab`))
+  const currentNavTab = navTabs[tabText] || (navTabs[tabText] = document.getElementById(`${tabText}-nav`))
+  currentNavTab.onclick = (e) => setActive(currentTab, currentNavTab)
+}
+
+setActive(tabs[current], navTabs[current])
