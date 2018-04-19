b03c797abe4d9acb282dd6e431621122249a46f9
Tim Froehlich <tjfroll@gmail.com>
2018-04-19 15:31:36 -0400

    Thumbnail

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const tabs = {}
 const navTabs = {}
 
-let current = 'experiments'
+let current = window.location.hash.slice(1) || 'experiments'
 
 const setActive = (currentTab, currentNavTab) => {
   for (const text in tabs) {
     const tab = tabs[text]
     const isCurrent = tab === currentTab
     tab.classList.toggle('active', isCurrent)
+    if (isCurrent)
+      window.location.hash = text
   }
   for (const text in navTabs) {
     const tab = navTabs[text]
